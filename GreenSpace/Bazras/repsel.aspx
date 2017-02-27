73b4e9006a99a0e2132b1351b8284934dabf8686
mohammadaqa1369 <mohammadaqa1369@gmail.com>
2017-02-28 00:18:07 +0330

    10/12/95

---
+++ GreenSpace/Bazras/repsel.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="repsel.aspx.cs"  MasterPageFile="~/Report/RepMaster.master" Inherits="GreenSpace.Bazras.repsel" %>
+
+
+<%@ Register Src="~/Report/CtlFromDateToDate.ascx" TagPrefix="uc1" TagName="CtlFromDateToDate" %>
+<%@ Register Src="~/Controls/ctlArchiveDD.ascx" TagPrefix="uc1" TagName="ctlArchiveDD" %>
+<%@ Register Src="~/Controls/CtlAllSubjectExplan.ascx" TagPrefix="uc1" TagName="CtlAllSubjectExplan" %>
+<%@ Register Src="~/Controls/CtlFromToDate.ascx" TagPrefix="uc1" TagName="CtlFromToDate" %>
+
+
+<asp:Content runat="server" ContentPlaceHolderID="PesonContentPlaceHolder" ID="cont1">
+    <div>
+        <div runat="server" id="dAgree" visible="false" >
+            <table>
+                <tr>
+                    <td>قراردادها:</td>
+                    <td> <asp:DropDownList runat="server" ID="ddAgree" AutoPostBack="True" OnSelectedIndexChanged="dd2Agree_SelectedIndexChanged"></asp:DropDownList></td>
+                   
+                </tr>
+                
+                <tr>
+<td></td>
+                </tr> 
+            </table>
+<uc1:ctlArchiveDD runat="server" id="ctlArchiveDD" /><br />
+            </div>
+
+
+>
+
+        <div id="dsubject1" runat="server" visible="false">
+
+         <uc1:CtlFromToDate runat="server" id="CtlFromToDate" /><br />
+             <uc1:CtlAllSubjectExplan runat="server" ID="CtlAllSubjectExplan" />
+        </div>
+
+         <br />
+        <asp:Button runat="server" SkinID="btnPrn" ID="btnprint21" OnClick="btnprint21_Click" CssClass="DTTT_Print" Text="پرینت" />
+  
+
+
+
+    </div>
+     <center>
+  <div style="width:100%;text-align:center" >
+               
+<iframe  id="MyIfarm1" runat="server" visible="false" width="100%" height="400px" >
+
+</iframe>
+
+ </div>
+
+     </center>
+</asp:Content>
+ 
+<%--<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <uc1:CtlFromDateToDate runat="server" id="CtlFromDateToDate" />
+        <asp:Button runat="server" ID="btnprint" OnClick="btnprint_Click" CssClass="DTTT_Print" Text="پرینت" />
+
+    </div>
+    </form>
+</body>
+</html>--%>
---
+++ GreenSpace/Bazras/repsel.aspx.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using GreenSpaceCL;
+using GreenSpaceDAL;
+using System.Web.UI.HtmlControls;
+
+using TaxiDAL;
+using TaxiCL;
+
+
+
+namespace GreenSpace.Bazras
+{
+    public partial class repsel : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+            if (!Page.IsPostBack)
+            {
+                dAgree.Visible = false;
+                if (Request.QueryString["RName"] == null)
+                    return;
+
+                if (Request.QueryString["RName"].ToString() == "Rp_SubjectPrice"
+                    || Request.QueryString["RName"].ToString() == "Rp_Percent"
+                    || Request.QueryString["RName"].ToString() == "Rp_Area"
+                    || Request.QueryString["RName"].ToString() == "Rp_Price2"
+                     || Request.QueryString["RName"].ToString() == "Rp_Khesarat"
+                     || Request.QueryString["RName"].ToString() == "rp_khesart3"
+                     || Request.QueryString["RName"].ToString() == "Rp_Park"
+                     || Request.QueryString["RName"].ToString() == "Rp_ExplainPrice2"
+
+                    )
+                {
+                    ClAgreement cl = new ClAgreement();
+                    ddAgree.DataSource = AgreementClass.GetList(cl);
+                    ddAgree.DataTextField = "AggreeName";
+                    ddAgree.DataValueField = "AgreementID";
+                    ddAgree.DataBind();
+                    ddAgree.Items.Insert(0, new ListItem("پیش فرض", "0"));
+                    dAgree.Visible = true;
+                }
+                ctlArchiveDD.BindDD();
+
+
+                if (Request.QueryString["RName"].ToString() == "SumPay" ||
+                    Request.QueryString["RName"].ToString() == "SumPay2" ||
+                    Request.QueryString["RName"].ToString() == "RptSumPayPage"
+                    )
+                {
+                    dsubject1.Visible = true;
+                    CtlAllSubjectExplan.BindAll = "1";
+                    CtlAllSubjectExplan.BindDD();
+                }
+            }
+
+        }
+
+
+
+        private DataSet GenrateDS(int? AgreeID = null, string fromdate = null, string todate = null, string repname = "")
+        {
+            SqlConnection cnn = new SqlConnection(CSharp.PublicFunction.cnstr());
+
+            SqlCommand cmd = new SqlCommand(repname, new SqlConnection(CSharp.PublicFunction.cnstr()));
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+
+            cmd.Parameters.Add(new SqlParameter("AgreeID", SqlDbType.Int)).Value = Securenamespace.SecureData.CheckSecurity(AgreeID);
+            cmd.Parameters.Add(new SqlParameter("FromDate", SqlDbType.NVarChar)).Value = Securenamespace.SecureData.CheckSecurity(fromdate);
+            cmd.Parameters.Add(new SqlParameter("ToDate", SqlDbType.NVarChar)).Value = Securenamespace.SecureData.CheckSecurity(todate);
+            //SqlParameter prmResult = new SqlParameter("Result", SqlDbType.Int);
+            //prmResult.Direction = ParameterDirection.Output;
+            //cmd.Parameters.Add(prmResult);
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            try
+            {
+                cnn.Open();
+                da.Fill(ds);
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            finally
+            {
+                cnn.Close();
+            }
+
+        }
+
+
+
+        protected void btnprint21_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                DataSet ds = null;
+                if (Request.QueryString["RName"].ToString() == "Rp_ExplainPrice2" ||
+                    Request.QueryString["RName"].ToString() == "Rp_SubjectPrice"
+                     )
+                {
+                    ClArchiveAgree cl = new ClArchiveAgree();
+                    cl.AgreeID = Convert.ToInt32(ddAgree.SelectedValue);
+                    cl.AcrchiveID = ctlArchiveDD.SelectedValue;
+                    if (Request.QueryString["RName"].ToString() == "Rp_ExplainPrice2")
+                        ds = ArchiveAgreeClass.GetListReport(cl);
+                    else if (Request.QueryString["RName"].ToString() == "Rp_SubjectPrice")
+                        ds = AgreementClass.GetListSubjectPrice(cl);
+                }
+                else if (Request.QueryString["RName"].ToString() == "SumPay" ||
+                    Request.QueryString["RName"].ToString() == "SumPay2" ||
+                    Request.QueryString["RName"].ToString() == "RptSumPayPage")
+                {
+                    ClExplanSubject cl2 = new ClExplanSubject();
+                    cl2.ExplainSubjectID = Convert.ToInt32(CtlAllSubjectExplan.SelectedValue);
+                    cl2.SubjectID = Convert.ToInt32(CtlAllSubjectExplan.subjectID);
+                    cl2.FromDate = CtlFromToDate.FromDate;
+                    cl2.ToDate = CtlFromToDate.ToDate;
+                    ds = ExplanSubjectClass.GetListSumPay(cl2, 0);
+                }
+                else if (Request.QueryString["RName"].ToString() == "Rp_Khesarat" || Request.QueryString["RName"].ToString() == "rp_khesart3")
+                {
+                    ClAgreementFine cl = new ClAgreementFine();
+                    cl.AgreementID = Convert.ToInt32(ddAgree.SelectedValue);
+                    cl.ArchivID = Convert.ToInt32(ctlArchiveDD.SelectedValue);
+                    ds = AgreementFineClass.GetList_Rep(cl);
+                }
+
+
+
+
+                Session["ReportData"] = ds;
+                MyIfarm1.Visible = true;
+
+                MyIfarm1.Attributes.Add("src", "~/Report/reportview2.aspx?RName=" + Request.QueryString["RName"].ToString() + "&PName=" + Request.QueryString["PName"].ToString());
+
+            }
+            catch { }
+
+        }
+
+
+
+        protected void dd2Agree_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ctlArchiveDD.Agreement = Convert.ToInt32(ddAgree.SelectedValue.ToString());
+            ctlArchiveDD.BindDD();
+        }
+
+
+
+    }
+}
